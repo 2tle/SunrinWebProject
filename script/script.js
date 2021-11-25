6574399cbcee887da4a24e4cb7b23e0146d748f7
Hyunju Yang <sw4019@naver.com>
2021-11-25 02:26:58 +0000

    news

--- script/script.js
+++ script/script.js
@@ -2,11 +2,27 @@ jQuery(document).ready(function() {
 	$('.imglist .item:gt(0)').hide();
     setInterval(function() {
         $('.imglist .item:first-child').fadeOut().next('.item').fadeIn().end().appendTo('.imglist')
-    }, 3000);
+    }, 6000);
 });
 
 function newsCrowl() {
-	fetch("https://sunrinjbtsbackend.2tle.repl.co/news").then(function(response) {
-		console.log(response)
-	})
+	var xmlHttp = new XMLHttpRequest();   
+	xmlHttp.onreadystatechange = function() {
+    if(this.status == 200 && this.readyState == this.DONE) {
+		var htmld ="";
+		var json = JSON.parse(xmlHttp.responseText);
+		var len = (json.news.length < 5) ? json.news.length : 5;
+        for(var i = 0; i< len; i++) {
+			var text = "<li><a href='"+json.news[i].link+"'>"+json.news[i].title+"</a></li>";
+			htmld += text;
+		}
+		document.getElementById('news-cro').innerHTML = htmld;
+
+    }
+
+};
+
+xmlHttp.open("GET", "https://sunrinjbtsbackend.2tle.repl.co/news", true);
+
+xmlHttp.send();
 }
