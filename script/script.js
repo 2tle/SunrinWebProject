c92a930fe2232e8e312b1166b4ea87920f8a5d2c
Hyunju Yang <sw4019@naver.com>
2021-12-01 16:21:54 +0000

    final i dont do

--- script/script.js
+++ script/script.js
@@ -2,7 +2,7 @@ jQuery(document).ready(function() {
 	$('.imglist .item:gt(0)').hide();
     setInterval(function() {
         $('.imglist .item:first-child').fadeOut().next('.item').fadeIn().end().appendTo('.imglist')
-    }, 3000);
+    }, 6000);
 });
 
 function newsCrowl() {
